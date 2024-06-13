f3dcfe65a30330f983a4de4ca39cd24ef08410ad
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-13 15:16:09 -0300

    Atualizando projeto

--- Assets/ControlaJogador.cs
+++ Assets/ControlaJogador.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        transform.translate
+        transform.Translate(Vector3.forward);
     }
 }
