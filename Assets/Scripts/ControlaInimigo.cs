773c7cfda22b0fd258fd98f485b229837d3de164
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-15 10:50:49 -0300

    Zumbi atacando

--- Assets/Scripts/ControlaInimigo.cs
+++ Assets/Scripts/ControlaInimigo.cs
@@ -23,15 +23,17 @@ public class ControlaInimigo : MonoBehaviour
     void FixedUpdate()
     {
         float distancia = Vector3.Distance(transform.position, Jogador.transform.position);
-    
+        Vector3 direcao = Jogador.transform.position - transform.position;
+        Quaternion novaRotacao = Quaternion.LookRotation(direcao);
+            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+
         if (distancia > 2.5) {
-            Vector3 direcao = Jogador.transform.position - transform.position;
             GetComponent<Rigidbody>().MovePosition(
                 GetComponent<Rigidbody>().position + 
                 direcao.normalized * VELOCIDADE_INIMIGO * Time.deltaTime);
-            
-            Quaternion novaRotacao = Quaternion.LookRotation(direcao);
-            GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+            GetComponent<Animator>().SetBool("Atacando", false);
+        } else {
+            GetComponent<Animator>().SetBool("Atacando", true);
         }
     }
 }
